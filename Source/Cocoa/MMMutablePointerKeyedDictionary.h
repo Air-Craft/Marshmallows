6841f6d46c4ed739a9ca1a8f114fe4dbb1470c73
Hari Karam Singh <harikaram@club15cc.com>
2012-02-08 18:57:27 +0000

    Fast enum support and safe key retention in MMMutable...Dict...

--- Source/Cocoa/MMMutablePointerKeyedDictionary.h
+++ Source/Cocoa/MMMutablePointerKeyedDictionary.h
@@ -4,10 +4,15 @@
  An incomplete implementation which casts UITouch objects used as keys using NSValue:valueFromPointer to allow
  them to be safely used as MutableDictionary keys.
  */
-@interface MMMutablePointerKeyedDictionary : NSObject
+@interface MMMutablePointerKeyedDictionary : NSObject <NSFastEnumeration>
 {
     NSMutableDictionary *dict;
+    NSMutableSet *retainer;
+    
+    unsigned long mutationCount;
 }
+
++ (id)dictionary;
 
 - (void)setObject:(id)anObject forKey:(id)aKey;
 - (id)objectForKey:(id)aKey;
--- Source/Cocoa/MMMutablePointerKeyedDictionary.m
+++ Source/Cocoa/MMMutablePointerKeyedDictionary.m
@@ -6,24 +6,34 @@
 
 @implementation MMMutablePointerKeyedDictionary
 
++ (id)dictionary 
+{
+    return [[self alloc] init];
+}
+
+/////////////////////////////////////////////////////////////////////////
+
 - (id)init
 {
     if (self = [super init]) {
         dict = [[NSMutableDictionary alloc] init]; 
+        retainer = [NSMutableSet set];
     }
     return self;
 }
 
-/**********************************************************************/
+/////////////////////////////////////////////////////////////////////////
 
 - (void)setObject:(id)anObject forKey:(id)aKey
 {   
+    mutationCount++;
     // Get an NSValue for the pointer address
     NSValue *ptrVal = [NSValue valueWithPointer:(__bridge const void *)aKey];
     [dict setObject:anObject forKey:ptrVal];
+    [retainer addObject:aKey];
 }
 
-/**********************************************************************/
+/////////////////////////////////////////////////////////////////////////
 
 - (id)objectForKey:(id)aKey
 {
@@ -32,25 +42,47 @@
     return [dict objectForKey:ptrVal];    
 }
 
-/**********************************************************************/
+/////////////////////////////////////////////////////////////////////////
 
 - (void)removeObjectForKey:(id)aKey 
 {
+    mutationCount++;
     [dict removeObjectForKey:[NSValue valueWithPointer:(__bridge const void *)aKey]];
+    [retainer removeObject:aKey];
 }
 
-/**********************************************************************/
+/////////////////////////////////////////////////////////////////////////
 
 - (void)removeObjectsForKeys:(NSArray *)keyArray
 {
+    mutationCount++;
     // Convert key array to NSValue with pointers array
-    NSMutableArray *newKeys;
     for (id key in keyArray) {
-        [newKeys addObject:[NSValue valueWithPointer:(__bridge const void *)key]];
+        [self removeObjectForKey:key];
     }
-    [dict removeObjectsForKeys:newKeys];
 }
 
+/////////////////////////////////////////////////////////////////////////
 
+- (NSUInteger)countByEnumeratingWithState:(NSFastEnumerationState *)state objects:(__unsafe_unretained id [])buffer count:(NSUInteger)len
+{
+    if (state->state >= dict.count) {
+        return 0;
+    }
+    
+    // Convert the keys to a C array
+    NSUInteger i = 0;
+    __unsafe_unretained id *objects; 
+    objects = (__unsafe_unretained id *)malloc(sizeof(id) * dict.count);
+    for (NSValue *v in [dict allKeys]) {
+        objects[i++] = (__unsafe_unretained id)[v pointerValue];
+    }
+    
+    state->itemsPtr = objects;
+    state->state = i;
+    state->mutationsPtr = &mutationCount;
+    
+    return i;
+}
 
 @end
