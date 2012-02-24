1fe8feddfef41ee993447d68ddf05c06a5d687af
Hari Karam Singh <harikaram@club15cc.com>
2012-02-24 16:11:26 +0000

    NSError category

---
+++ Source/Cocoa/NSError+Marshmallows.h
@@ -0,0 +1,13 @@
+//
+//  NSError+MMAudioSessionErrors.h
+//  SoundWand
+//
+//  Created by  on 20/02/2012.
+//  Copyright (c) 2012 Club 15CC. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSError (MMAudioSessionErrors)
+
+@end
---
+++ Source/Cocoa/NSError+Marshmallows.m
@@ -0,0 +1,13 @@
+//
+//  NSError+MMAudioSessionErrors.m
+//  SoundWand
+//
+//  Created by  on 20/02/2012.
+//  Copyright (c) 2012 Club 15CC. All rights reserved.
+//
+
+#import "NSError+MMAudioSessionErrors.h"
+
+@implementation NSError (MMAudioSessionErrors)
+
+@end
