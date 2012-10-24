1be1583dd671be019e271c9fbe9a8880bad04e1c
Hari Karam Singh <harikaram@club15cc.com>
2012-10-24 10:42:35 +0100

    Ready to test AUMFileRecordingRenderer

--- Source/AudioMarshmallows/AUMFileRecordingRenderer.h
+++ Source/AudioMarshmallows/AUMFileRecordingRenderer.h
@@ -11,22 +11,49 @@
 #import <AudioToolbox/AudioToolbox.h>
 #import "AUMRendererProtocol.h"
 #import "AUMTypes.h"
+#import "Private/AUMAtomicType.h"
 
 /**
- \brief 
+ \brief Records audio from an AUMUnit bus to disk
+ 
+ \section DEV NOTES
+ - Only use _stopRequestFlag in the RCB and in [stop].  [stop] is a synchronous command and waits for the RCB to finish so no need to check it outside of these locations.  Also use [stop] interally to stop recording as it handles the whole delay thing invisibly.
  */
 @interface AUMFileRecordingRenderer : NSObject <AUMRendererProtocol>
+{
+    /// Naughty publics to allow RCB access without expensive ObjC messaging
+@public
+    /// Set to true to request that the RCB stop the recording and set isRecording=false after next iteration.
+    AUM::AtomicBool _stopRequestFlag;
+    // Confirm
+    AUM::AtomicBool _isRecording;
+    ExtAudioFileRef _fileRef;
+}
 
 /** Setting opens a new file for output
  \throws kAUMAudioFileException on error closing any previous file or creating the new one
  */
 @property (nonatomic, strong, readonly) NSURL *outputFileURL;
 @property (nonatomic) AudioStreamBasicDescription inputStreamFormat;
-
+@property (nonatomic, readonly) AUMAudioFileFormatDescription outputFileFormat;
 
 - (void)newOutputFileWithURL:(NSURL *)aURL withFileFormat:(AUMAudioFileFormatDescription)aFileFormat;
 
+
+/** Open the requested file and prepare for recording.  Called automatically by record if not alreayd queued but can be called manually to have record kick in faster 
+ \throws NSInternalInconsistencyException If no file specified
+ */
+- (void)queue;
+
+/** Begin recording to the file.  If file has not ben "queued" then queueFile will be called
+ \throws NSInternalInconsistencyException If already recording
+ \throws NSInternalInconsistencyException If no file has been set
+ */
 - (void)record;
+
+/** Stops the recording. Also closes the audio file to ensure all async writes have completed and that the file can be used elsewhere
+ \throws NSInternalInconsistencyException if already stopped
+ */
 - (void)stop;
 
 
--- Source/AudioMarshmallows/AUMFileRecordingRenderer.mm
+++ Source/AudioMarshmallows/AUMFileRecordingRenderer.mm
@@ -5,8 +5,11 @@
  @{ 
  */
 
+#import <unistd.h>
 #import "AUMFileRecordingRenderer.h"
 #import "Private/AUMErrorChecking.h"
+#import "MarshmallowCocoa.h"
+#import "MarshmallowDebug.h"
 
 /////////////////////////////////////////////////////////////////////////
 #pragma mark - RCB
@@ -18,9 +21,24 @@ static OSStatus AUMFileRecordingRendererRCB(void *							inRefCon,
                                              UInt32							inNumberFrames,
                                              AudioBufferList *				ioData)
 {
+    AUMFileRecordingRenderer *THIS = (__bridge AUMFileRecordingRenderer *)inRefCon;
     
+    if (not THIS->_isRecording)
+        return noErr;
     
-    return noErr;
+    OSStatus res = ExtAudioFileWriteAsync(THIS->_fileRef, inNumberFrames, ioData);
+    
+    if (res != noErr) {
+        MMLogRealTime(@"%@", [NSString mm_ErrorCodeStringFromOSStatus:res]);
+    }
+    
+    // All done, so disable recording if requested
+    if (THIS->_stopRequestFlag) {
+        THIS->_stopRequestFlag = false;
+        THIS->_isRecording = false;
+    }
+    
+    return res;
 }
 
 
@@ -30,7 +48,8 @@ static OSStatus AUMFileRecordingRendererRCB(void *							inRefCon,
 
 @implementation AUMFileRecordingRenderer
 {
-    ExtAudioFileRef _fileRef;
+    
+
 }
 
 /////////////////////////////////////////////////////////////////////////
@@ -43,10 +62,18 @@ static OSStatus AUMFileRecordingRendererRCB(void *							inRefCon,
     if (self) {
         _fileRef = NULL;    // Init just to be sure
         _inputStreamFormat = kAUMStreamFormatAUMUnitCanonical;  // Just a handy default
+        _stopRequestFlag = false;
+        _isRecording = false;
     }
     return self;
 }
 
+/////////////////////////////////////////////////////////////////////////
+
+- (void)dealloc
+{
+    if (_isRecording) [self stop];  // Disposes of the ExtAudioFile as well
+}
 
 /////////////////////////////////////////////////////////////////////////
 #pragma mark - Properties
@@ -78,14 +105,29 @@ static OSStatus AUMFileRecordingRendererRCB(void *							inRefCon,
 #pragma mark - Public API
 /////////////////////////////////////////////////////////////////////////
 
-
 - (void)newOutputFileWithURL:(NSURL *)aURL withFileFormat:(AUMAudioFileFormatDescription)aFileFormat
 {
+    if (_isRecording)
+        [self stop];
+    
     // Close the old one
     if (_fileRef) {
         _(ExtAudioFileDispose(_fileRef),
           kAUMAudioFileException,
           @"Error disposing of previous file %@", _outputFileURL.lastPathComponent);
+    }
+    
+    // Store the parameters for opening on queue...
+    _outputFileURL = aURL;
+    _outputFileFormat = aFileFormat;
+}
+
+/////////////////////////////////////////////////////////////////////////
+
+- (void)queue
+{
+    if (!_outputFileURL) {
+        [NSException raise:NSInternalInconsistencyException format:@"Output file must be loaded first"];
     }
     
     // Open the new one
@@ -94,46 +136,64 @@ static OSStatus AUMFileRecordingRendererRCB(void *							inRefCon,
     asbd.mFormatID = kAudioFormatAppleIMA4;
     asbd.mChannelsPerFrame = 2;
     
-    _(ExtAudioFileCreateWithURL((__bridge CFURLRef)aURL,
-                                aFileFormat.fileTypeId,
-                                &aFileFormat.streamFormat,
+    _(ExtAudioFileCreateWithURL((__bridge CFURLRef)_outputFileURL,
+                                _outputFileFormat.fileTypeId,
+                                &_outputFileFormat.streamFormat,
                                 NULL,
                                 kAudioFileFlags_EraseFile,
                                 &_fileRef),
       kAUMAudioFileException,
-      @"Error creating audio file %@", aURL.absoluteString);
-    
-    
-    // Now set our property as we've opened it successfully
-    _outputFileURL = aURL;
-    
-    
+      @"Error creating audio file %@", _outputFileURL.absoluteString);
+  
     // Explicitly set the encoding method to prevent various issues
     // See http://michaelchinen.com/2012/04/30/ios-encoding-to-aac-with-the-extended-audio-file-services-gotchas/
+    
     _(ExtAudioFileSetProperty(_fileRef,
                               kExtAudioFileProperty_CodecManufacturer,
-                              sizeof(aFileFormat.codecManufacturer),
-                              &aFileFormat.codecManufacturer),
+                              sizeof(_outputFileFormat.codecManufacturer),
+                              &_outputFileFormat.codecManufacturer),
       kAUMAudioFileException,
       @"Error setting the codec manufacturer (hardware/software) for audio file %@", _outputFileURL.absoluteString);
 }
 
 /////////////////////////////////////////////////////////////////////////
 
-
-
 - (void)record
 {
-    // Check input format has been set
+    if (_isRecording) {
+        [NSException raise:NSInternalInconsistencyException format:@"Recording is already on!"];
+    }
+    if (!self._isQueued)
+        [self queue];
+    
+    _isRecording = true;
 }
 
+/////////////////////////////////////////////////////////////////////////
+
+- (void)stop
+{
+    if (not _isRecording) {
+        [NSException raise:NSInternalInconsistencyException format:@"Recording is already stopped!"];
+    }
+    
+    // Request stop and then spin lock until the RCB confirms
+    _stopRequestFlag = true;
+    while (_isRecording) sleep(5);
+    
+    // Reset the request flag
+    _stopRequestFlag = false;
+    
+    ExtAudioFileDispose(_fileRef);
+    _fileRef = NULL;
+}
 
 
 /////////////////////////////////////////////////////////////////////////
 #pragma mark - Private API
 /////////////////////////////////////////////////////////////////////////
 
-
+- (BOOL)_isQueued { return _fileRef != NULL; }
 
 @end
 
